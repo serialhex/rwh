176198cbeb92e2f69253696e8d0d4a4fe5989f23
Justin Patera <serialhex@gmail.com>
2011-11-12 08:58:05 -0500

    on chapter 3 now!!

---
+++ ch03/BookStore.hs
@@ -0,0 +1,12 @@
+-- file: ch03/BookStore.hs
+data BookInfo = Book Int String [String]
+                deriving (Show)
+
+data MagazineInfo = Magazine Int String [String]
+                    deriving (Show)
+
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
+
+data BookReview = BookReview BookInfo CustomerID String
+
